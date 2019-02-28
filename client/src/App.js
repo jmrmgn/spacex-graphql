50d91747e0f03caecd6d6d6d61b7b60b7b39d5e7
Jomar <jomar.magno026@gmail.com>
2019-02-28 11:32:51 +0800

    client setup

---
+++ client/src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import logo from './logo.jpg';
+import './App.css';
+
+class App extends Component {
+   render() {
+      return (
+         <div className="App">
+            <img src={logo} alt="SpaceX" style={{ width: 300, display: 'block', margin: 'auto' }} />
+         </div>
+      );
+   }
+}
+
+export default App;
