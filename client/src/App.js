9b296ff1823a5aee84d7c60cd4d164a94077ea7c
Jomar <jomar.magno026@gmail.com>
2019-02-28 13:00:48 +0800

    react apollo setup

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,25 @@
 import React, { Component } from 'react';
+import ApolloClient from 'apollo-boost';
+import { ApolloProvider } from 'react-apollo';
+
+import Launches from './components/Launches/Launches';
+
 import logo from './logo.jpg';
 import './App.css';
+
+const client = new ApolloClient({
+   uri: 'http://localhost:5000/graphql'
+});
 
 class App extends Component {
    render() {
       return (
-         <div className="App">
-            <img src={logo} alt="SpaceX" style={{ width: 300, display: 'block', margin: 'auto' }} />
-         </div>
+         <ApolloProvider client={client}>
+            <div className="container">
+               <img src={logo} alt="SpaceX" style={{ width: 300, display: 'block', margin: 'auto' }} />
+               <Launches />
+            </div>
+         </ApolloProvider>
       );
    }
 }
---
+++ client/src/components/Launches/LaunchItem.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const launchItem = props => {
+   const { flight_number, mission_name, launch_date_local, launch_success } = props.launch;
+
+   return (
+      <div className="card card-body mb-3">
+         <div className="row">
+            <div className="col-md-9">
+               <h4>Mission: {mission_name} </h4>
+               <p>Date: {launch_date_local}</p>
+            </div>
+            <div className="col-md-3">
+               <button className="btn btn-secondary">Launch Details</button>
+            </div>
+         </div>
+      </div>
+   );
+};
+
+launchItem.propTypes = {
+   launch: PropTypes.object.isRequired
+};
+
+export default launchItem;
